b7b61607fbbf898de4396ff3acad30aec5d8b6a9
Wynn Chen <wynn987@gmail.com>
2018-05-14 11:27:21 +0800

    updated test scripts

--- init-test.sh
+++ init-test.sh
@@ -1,4 +1,4 @@
 docker stop greview-frontend:latest
 docker system prune -a -f
 docker build -t greview-frontend:latest --no-cache .
-docker run -it greview-frontend:latest npm test
+docker run greview-frontend:latest npm test
