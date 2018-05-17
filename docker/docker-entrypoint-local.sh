888e5c547c15cb110230b9710e1339e961cf3e99
Wynn Chen <wynn987@gmail.com>
2018-05-17 10:24:40 +0800

    added integration scripts

---
+++ docker/docker-entrypoint-local.sh
@@ -0,0 +1,8 @@
+token=$(curl \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"bgp","password":"test12"}' \
+  http://localhost:3000/api/v1/oauth/token |\
+  jq -r '.access_token')
+echo "REACT_APP_AUTH_TOKEN=\"Bearer ${token}\"" > .env.local
+echo "NODE_ENV=development" >> .env.local
