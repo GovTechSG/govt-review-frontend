d2e468f012b30087c728044ab4aa07ad5d294ade
Wynn Chen <wynn987@gmail.com>
2018-06-21 15:10:04 +0800

    added token curl

--- docker/docker-entrypoint-prod.sh
+++ docker/docker-entrypoint-prod.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+token=$(curl \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --data "{'name':'${name}','password':'${password}'}" \
+  http://$(getent hosts veracity_backend | awk '{ print $1 }'):3000/api/v1/oauth/token |\
+  jq -r '.access_token')
+echo "REACT_APP_AUTH_TOKEN=\"Bearer ${token}\"" > .env.local
+echo "REACT_APP_BACKEND_ENV=production" > .env.production
+echo "HOST=review-api.gds-gov.tech" >> .env.production
+
 npm run build --production
 serve -l 5000 -s build
