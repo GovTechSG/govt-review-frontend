7480ee660144b26fe61ab42109ecd841851b4b56
Jonathan Tan <jonathantan@as-MacBook-Pro.local>
2018-06-27 16:55:09 +0800

    Add integration test for offerings

--- src/components/company/CompanyPage/CompanyReviews/CompanyReviews.js
+++ src/components/company/CompanyPage/CompanyReviews/CompanyReviews.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import { FormattedMessage } from 'react-intl';
-import { Row, Col } from 'react-bootstrap';
+import { FormattedMessage } from 'react-intl';
+import { Row, Col, ProgressBar } from 'react-bootstrap';
 import API from '../../../../_utilities/api';
 import { API_URL_PREFIX } from '../../../../_utilities/api_url_prefix';
 import PageLoadSpinner from '../../../animation/PageLoadSpinner';
@@ -10,7 +10,12 @@ export default class CompanyReviews extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviewData: null,
+      reviewData: {
+        review_count: 10,
+        positive: 5,
+        neutral: 3,
+        negative: 2
+      },
     };
   }
 
@@ -22,9 +27,9 @@ export default class CompanyReviews extends Component {
       data: {}
     }).then(reviewData => {
       this.setState({ reviewData });
-    }).fail(() => {
-      const reviewData = 'Fail';
-      this.setState({ reviewData });
+    // }).fail(() => {
+    //   const reviewData = 'Fail';
+    //   this.setState({ reviewData });
     });
   }
 
@@ -35,11 +40,6 @@ export default class CompanyReviews extends Component {
   }
 
   render() {
-    // <ProgressBar>
-    //   <ProgressBar bsStyle="success" max={data.reviews_count} now={data.positive} key={1} />
-    //   <ProgressBar bsStyle="warning" max={data.reviews_count} now={data.neutral} key={2} />
-    //   <ProgressBar bsStyle="danger" max={data.reviews_count} now={data.negative} key={3} />
-    // </ProgressBar>
     if (!this.state.reviewData) {
       return (
         <div className="page-load-spinner">
@@ -52,11 +52,57 @@ export default class CompanyReviews extends Component {
         <div />
       );
     }
+    const { reviewData } = this.state;
     return (
       <div className="vendor-card">
         <Col xs={12}>
           <Row className="vendor-item">
-
+            <Col xs={2}>
+              <div className="rating-box">
+              </div>
+            </Col>
+            <Col xs={3} style={{ fontSize: '12px' }}>
+              <Row>
+                <Col xs={12}>
+                </Col>
+              </Row>
+              {this.state.reviewData.positive &&
+              <Row>
+                <Col xs={3}>
+                  <FormattedMessage id="companyreviews.positive" />
+                </Col>
+                <Col xs={7}>
+                  <ProgressBar max={reviewData.review_count} now={reviewData.positive} key={1} />
+                </Col>
+                <Col xs={2}>
+                  {Math.round(reviewData.positive / reviewData.review_count * 100)}%
+                </Col>
+              </Row>}
+              {this.state.reviewData.positive &&
+              <Row>
+                <Col xs={3}>
+                  <FormattedMessage id="companyreviews.neutral" />
+                </Col>
+                <Col xs={7}>
+                  <ProgressBar max={reviewData.review_count} now={reviewData.neutral} key={1} />
+                </Col>
+                <Col xs={2}>
+                  {Math.round(reviewData.neutral / reviewData.review_count * 100)}%
+                </Col>
+              </Row>}
+              {this.state.reviewData.positive &&
+              <Row>
+                <Col xs={3}>
+                  <FormattedMessage id="companyreviews.negative" />
+                </Col>
+                <Col xs={7}>
+                  <ProgressBar max={reviewData.review_count} now={reviewData.negative} key={1} />
+                </Col>
+                <Col xs={2} >
+                  {Math.round(reviewData.negative / reviewData.review_count * 100)}%
+                </Col>
+              </Row>}
+            </Col>
           </Row>
         </Col>
       </div>
