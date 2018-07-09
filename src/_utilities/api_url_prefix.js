9845fcd268fab46af23cf933a4d581c6e7bcfeac
Wynn Chen <wynn987@gmail.com>
2018-07-09 14:28:50 +0800

    changed local deployment script

--- src/_utilities/api_url_prefix.js
+++ src/_utilities/api_url_prefix.js
@@ -8,5 +8,8 @@ if (process.env.REACT_APP_BACKEND_ENV === 'qa') {
 if (process.env.REACT_APP_BACKEND_ENV === 'development') {
   prefix = 'http://localhost:3000';
 }
+if (process.env.REACT_APP_BACKEND_ENV === 'docker') {
+  prefix = '172.18.0.4:3000';
+}
 
 export const API_URL_PREFIX = prefix;
